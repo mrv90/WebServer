d9eb3e914201c6eafbbbca56f17e20b41e65bb38
mrv90 <hamid.mousavinasab@gmail.com>
2019-12-04 11:23:14 +0330

    برای مرتب کردن بیشتر تستها، یک فولدر ایجاد شد و برمبنای نام آن، namespace افزوده شد

--- src/Test/Crud.cpp
+++ src/Test/Crud.cpp
@@ -3,167 +3,169 @@
 #include "TestServer.h"
 
 namespace Test {
-	class Crud : public ::testing::Test {
-	protected:
-		web::uri local = uri_builder(U("http://127.0.0.1:34568")).to_uri();
-		TestServer srv = TestServer(local, true);
-		BackEnd::Net::Client cli = BackEnd::Net::Client(local);
+	namespace End2End {
+		class Crud : public ::testing::Test {
+		protected:
+			web::uri local = uri_builder(U("http://127.0.0.1:34568")).to_uri();
+			TestServer srv = TestServer(local, true);
+			BackEnd::Net::Client cli = BackEnd::Net::Client(local);
 
-		virtual void SetUp() {
-			srv.open();
-		}
+			virtual void SetUp() {
+				srv.open();
+			}
 
-		virtual void TearDown() {
-			srv.close();
-		}
-	};
-	
-	// working with STUDENT entity
-	TEST_F(Crud, Student_OnCreating_Success) {
-		auto req = uri_builder(local).append_path(U("student")).append_query(U("name=Mohsen"))
-			.append_query(U("family_name=Shiri")).append_query(U("birth_date=13710101"));
-		cli.make_request(methods::POST, req.to_string(), 0).then([](http_response response) {
-			EXPECT_EQ(response.status_code(), status_codes::Created);
-		}).wait();
-	}
-	TEST_F(Crud, Student_OnSearching_Success) {
-		auto req = uri_builder(local).append_path(U("student")).append_query(U("family_name=Naseri"));
-		cli.make_request(methods::GET, req.to_string(), 0).then([](http_response response)
-		{
-			EXPECT_EQ(response.status_code(), status_codes::OK);
-			response.extract_json().then([](web::json::value result) {
-				web::json::value expected = web::json::value::parse(L"{\"student_id\": 1, \
-					\"name\": \"Ali\", \"family_name\": \"Naseri\", \"birth_date\": \"13700101\"}");
-				EXPECT_EQ(result.serialize(), expected.serialize());
+			virtual void TearDown() {
+				srv.close();
+			}
+		};
+
+		// working with STUDENT entity
+		TEST_F(Crud, Student_OnCreating_Success) {
+			auto req = uri_builder(local).append_path(U("student")).append_query(U("name=Mohsen"))
+				.append_query(U("family_name=Shiri")).append_query(U("birth_date=13710101"));
+			cli.make_request(methods::POST, req.to_string(), 0).then([](http_response response) {
+				EXPECT_EQ(response.status_code(), status_codes::Created);
 			}).wait();
-		}).wait();
-	}
-	TEST_F(Crud, Student_OnUpdating_Success) {
-		auto req = uri_builder(local).append_path(U("student")).append_query(U("family_name=Naseri"));
-		auto body = web::json::value(U("name=Alireza"));
-		cli.make_request(methods::PUT, req.to_string(), body).then([](http_response response) {
-			EXPECT_EQ(response.status_code(), status_codes::OK);
-		}).wait();
-	}
-	TEST_F(Crud, Student_OnPatching_Success) {
-		auto req = uri_builder(local).append_path(U("student")).append_query(U("family_name=Naseri"));
-		auto body = web::json::value(U("name=Alireza"));
-		cli.make_request(methods::PATCH, req.to_string(), body).then([](http_response response) {
-			EXPECT_EQ(response.status_code(), status_codes::OK);
-		}).wait();
-	}
-		
+		}
+		TEST_F(Crud, Student_OnSearching_Success) {
+			auto req = uri_builder(local).append_path(U("student")).append_query(U("family_name=Naseri"));
+			cli.make_request(methods::GET, req.to_string(), 0).then([](http_response response)
+			{
+				EXPECT_EQ(response.status_code(), status_codes::OK);
+				response.extract_json().then([](web::json::value result) {
+					web::json::value expected = web::json::value::parse(L"{\"student_id\": 1, \
+					\"name\": \"Ali\", \"family_name\": \"Naseri\", \"birth_date\": \"13700101\"}");
+					EXPECT_EQ(result.serialize(), expected.serialize());
+				}).wait();
+			}).wait();
+		}
+		TEST_F(Crud, Student_OnUpdating_Success) {
+			auto req = uri_builder(local).append_path(U("student")).append_query(U("family_name=Naseri"));
+			auto body = web::json::value(U("name=Alireza"));
+			cli.make_request(methods::PUT, req.to_string(), body).then([](http_response response) {
+				EXPECT_EQ(response.status_code(), status_codes::OK);
+			}).wait();
+		}
+		TEST_F(Crud, Student_OnPatching_Success) {
+			auto req = uri_builder(local).append_path(U("student")).append_query(U("family_name=Naseri"));
+			auto body = web::json::value(U("name=Alireza"));
+			cli.make_request(methods::PATCH, req.to_string(), body).then([](http_response response) {
+				EXPECT_EQ(response.status_code(), status_codes::OK);
+			}).wait();
+		}
 
-	// working with COURSE entity
-	TEST_F(Crud, Course_OnCreating_Success) {
-		auto req = uri_builder(local).append_path(U("course")).append_query(U("name=Geography"));
-		cli.make_request(methods::POST, req.to_string(), 0).then([](http_response response) {
-			EXPECT_EQ(response.status_code(), status_codes::Created);
-		}).wait();
-	}
-	TEST_F(Crud, Course_OnSearching_Success) {
-		auto req = uri_builder(local).append_path(U("course")).append_query(U("name=Physics"));
-		cli.make_request(methods::GET, req.to_string(), 0).then([](http_response response) {
-			EXPECT_EQ(response.status_code(), status_codes::OK);
-			response.extract_json().then([](web::json::value result) {
-				web::json::value expected = web::json::value::parse(L"{\"course_id\": 2, \"name\": \"Physics\"}");
-				EXPECT_EQ(result.serialize(), expected.serialize());
-			});
-		}).wait();
-	}
-	TEST_F(Crud, Course_OnUpdating_Success) {
-		auto req = uri_builder(local).append_path(U("course")).append_query(U("name=Math"));
-		auto body = web::json::value(U("name=Mathematics"));
-		cli.make_request(methods::PUT, req.to_string(), body).then([](http_response response) {
-			EXPECT_EQ(response.status_code(), status_codes::OK);
-		}).wait();
-	}
-		
 
-	// woring with CLASS entity
-	TEST_F(Crud, Class_OnCreating_Success) {
-		auto req = uri_builder(local).append_path(U("class")).append_query(U("course_ref=1"))
-			.append_query(U("student_ref=2")).append_query(U("score_ref=1"));
-		cli.make_request(methods::POST, req.to_string(), 0).then([](http_response response) {
-			EXPECT_EQ(response.status_code(), status_codes::Created);
-		}).wait();
-	}
-	TEST_F(Crud, Class_OnSearching_Success) {
-		auto req = uri_builder(local).append_path(U("class")).append_query(U("course_ref=1"));
-		cli.make_request(methods::GET, req.to_string(), 0).then([](http_response response) {
-			EXPECT_EQ(response.status_code(), status_codes::OK); 
-			response.extract_json().then([](web::json::value result) {
-				web::json::value expected = web::json::value::parse(L"{\"class_id\": 1, \
+		// working with COURSE entity
+		TEST_F(Crud, Course_OnCreating_Success) {
+			auto req = uri_builder(local).append_path(U("course")).append_query(U("name=Geography"));
+			cli.make_request(methods::POST, req.to_string(), 0).then([](http_response response) {
+				EXPECT_EQ(response.status_code(), status_codes::Created);
+			}).wait();
+		}
+		TEST_F(Crud, Course_OnSearching_Success) {
+			auto req = uri_builder(local).append_path(U("course")).append_query(U("name=Physics"));
+			cli.make_request(methods::GET, req.to_string(), 0).then([](http_response response) {
+				EXPECT_EQ(response.status_code(), status_codes::OK);
+				response.extract_json().then([](web::json::value result) {
+					web::json::value expected = web::json::value::parse(L"{\"course_id\": 2, \"name\": \"Physics\"}");
+					EXPECT_EQ(result.serialize(), expected.serialize());
+				});
+			}).wait();
+		}
+		TEST_F(Crud, Course_OnUpdating_Success) {
+			auto req = uri_builder(local).append_path(U("course")).append_query(U("name=Math"));
+			auto body = web::json::value(U("name=Mathematics"));
+			cli.make_request(methods::PUT, req.to_string(), body).then([](http_response response) {
+				EXPECT_EQ(response.status_code(), status_codes::OK);
+			}).wait();
+		}
+
+
+		// woring with CLASS entity
+		TEST_F(Crud, Class_OnCreating_Success) {
+			auto req = uri_builder(local).append_path(U("class")).append_query(U("course_ref=1"))
+				.append_query(U("student_ref=2")).append_query(U("score_ref=1"));
+			cli.make_request(methods::POST, req.to_string(), 0).then([](http_response response) {
+				EXPECT_EQ(response.status_code(), status_codes::Created);
+			}).wait();
+		}
+		TEST_F(Crud, Class_OnSearching_Success) {
+			auto req = uri_builder(local).append_path(U("class")).append_query(U("course_ref=1"));
+			cli.make_request(methods::GET, req.to_string(), 0).then([](http_response response) {
+				EXPECT_EQ(response.status_code(), status_codes::OK);
+				response.extract_json().then([](web::json::value result) {
+					web::json::value expected = web::json::value::parse(L"{\"class_id\": 1, \
 					\"course_ref\": 1, \"student_ref\": 1}");
-				EXPECT_EQ(result.serialize(), expected.serialize());
-			});
-		}).wait();
-	}
-	TEST_F(Crud, Class_OnUpdating_Success) {
-		auto req = uri_builder(local).append_path(U("class")).append_query(U("course_ref=1"));
-		auto body = web::json::value(U("student_ref=1"));
-		cli.make_request(methods::PUT, req.to_string(), body).then([](http_response response){
-			EXPECT_EQ(response.status_code(), status_codes::OK);
-		}).wait();
-	}
-	
-
-	// working with QUIZ entity 
-	TEST_F(Crud, Quiz_OnCreating_Success) {
-		auto req = uri_builder(local).append_path(U("quiz")).append_query(U("class_ref=2"))
-			.append_query(U("student_ref=2")).append_query(U("grade:18"));
-		cli.make_request(methods::POST, req.to_string(), 0).then([](http_response response) {
-			EXPECT_EQ(response.status_code(), status_codes::Created);
-		}).wait();
-	}
-	TEST_F(Crud, Quiz_OnSearching_Success) {
-		auto req = uri_builder(local).append_path(U("quiz")).append_query(U("class_ref=1"));
-		cli.make_request(methods::GET, req.to_string(), 0).then([](http_response response) {
-			response.extract_json().then([](web::json::value result) {
-				web::json::value expected = web::json::value::parse(L"{\"class_ref\":1,\"quiz_id\":1,\"grade\":17.5,\"student_ref\":1}");
-				EXPECT_EQ(result.serialize(), expected.serialize());
-			});
-		}).wait();
-	}
-	TEST_F(Crud, Quiz_OnUpdating_Success) {
-		auto req = uri_builder(local).append_path(U("quiz")).append_query(U("quiz_id=2"));
-		auto body = web::json::value(U("grade=20"));
-		cli.make_request(methods::PUT, req.to_string(), body).then([](http_response response) {
-			EXPECT_EQ(response.status_code(), status_codes::OK);
-		}).wait();
-	}
-	TEST_F(Crud, Quiz_OnDeleting_Success) {
-		auto req = uri_builder(local).append_path(U("quiz")).append_query(U("class_ref=1"));
-		cli.make_request(methods::DEL, req.to_string(), 0).then([](http_response response) {
-			EXPECT_EQ(response.status_code(), status_codes::OK);
-		}).wait();
-	}
+					EXPECT_EQ(result.serialize(), expected.serialize());
+				});
+			}).wait();
+		}
+		TEST_F(Crud, Class_OnUpdating_Success) {
+			auto req = uri_builder(local).append_path(U("class")).append_query(U("course_ref=1"));
+			auto body = web::json::value(U("student_ref=1"));
+			cli.make_request(methods::PUT, req.to_string(), body).then([](http_response response) {
+				EXPECT_EQ(response.status_code(), status_codes::OK);
+			}).wait();
+		}
 
 
-	// working with SCORE entity
-	TEST_F(Crud, Score_OnCreating_Success) {
-		auto req = uri_builder(local).append_path(U("score")).append_query(U("class_ref=1")).append_query(U("student_ref=2"))
-			.append_query(U("sem_avg=14")).append_query(U("mid_term=18")).append_query(U("final_term=11"));
-		cli.make_request(methods::POST, req.to_string(), 0).then([](http_response response) {
-			EXPECT_EQ(response.status_code(), status_codes::Created);
-		}).wait();
-	}
-	TEST_F(Crud, Score_OnSearching_Success) {
-		auto req = uri_builder(local).append_path(U("score")).append_query(U("class_ref=1"));
-		cli.make_request(methods::GET, req.to_string(), 0).then([](http_response response) {
-			EXPECT_EQ(response.status_code(), status_codes::OK); 
-			response.extract_json().then([](web::json::value result) {
-				web::json::value expected = web::json::value::parse(L"{\"score_id\": 1, \"class_ref\": 1, \"student_ref\": 1,\
+		// working with QUIZ entity 
+		TEST_F(Crud, Quiz_OnCreating_Success) {
+			auto req = uri_builder(local).append_path(U("quiz")).append_query(U("class_ref=2"))
+				.append_query(U("student_ref=2")).append_query(U("grade:18"));
+			cli.make_request(methods::POST, req.to_string(), 0).then([](http_response response) {
+				EXPECT_EQ(response.status_code(), status_codes::Created);
+			}).wait();
+		}
+		TEST_F(Crud, Quiz_OnSearching_Success) {
+			auto req = uri_builder(local).append_path(U("quiz")).append_query(U("class_ref=1"));
+			cli.make_request(methods::GET, req.to_string(), 0).then([](http_response response) {
+				response.extract_json().then([](web::json::value result) {
+					web::json::value expected = web::json::value::parse(L"{\"class_ref\":1,\"quiz_id\":1,\"grade\":17.5,\"student_ref\":1}");
+					EXPECT_EQ(result.serialize(), expected.serialize());
+				});
+			}).wait();
+		}
+		TEST_F(Crud, Quiz_OnUpdating_Success) {
+			auto req = uri_builder(local).append_path(U("quiz")).append_query(U("quiz_id=2"));
+			auto body = web::json::value(U("grade=20"));
+			cli.make_request(methods::PUT, req.to_string(), body).then([](http_response response) {
+				EXPECT_EQ(response.status_code(), status_codes::OK);
+			}).wait();
+		}
+		TEST_F(Crud, Quiz_OnDeleting_Success) {
+			auto req = uri_builder(local).append_path(U("quiz")).append_query(U("class_ref=1"));
+			cli.make_request(methods::DEL, req.to_string(), 0).then([](http_response response) {
+				EXPECT_EQ(response.status_code(), status_codes::OK);
+			}).wait();
+		}
+
+
+		// working with SCORE entity
+		TEST_F(Crud, Score_OnCreating_Success) {
+			auto req = uri_builder(local).append_path(U("score")).append_query(U("class_ref=1")).append_query(U("student_ref=2"))
+				.append_query(U("sem_avg=14")).append_query(U("mid_term=18")).append_query(U("final_term=11"));
+			cli.make_request(methods::POST, req.to_string(), 0).then([](http_response response) {
+				EXPECT_EQ(response.status_code(), status_codes::Created);
+			}).wait();
+		}
+		TEST_F(Crud, Score_OnSearching_Success) {
+			auto req = uri_builder(local).append_path(U("score")).append_query(U("class_ref=1"));
+			cli.make_request(methods::GET, req.to_string(), 0).then([](http_response response) {
+				EXPECT_EQ(response.status_code(), status_codes::OK);
+				response.extract_json().then([](web::json::value result) {
+					web::json::value expected = web::json::value::parse(L"{\"score_id\": 1, \"class_ref\": 1, \"student_ref\": 1,\
 					 \"sem_avg\": 12.5, \"mid_term\": 15, \"final_term\": 17}");
-				EXPECT_EQ(result.serialize(), expected.serialize());
-			});
-		}).wait();
-	}
-	TEST_F(Crud, Score_OnUpdating_Success) {
-		auto req = uri_builder(local).append_path(U("score")).append_query(U("class_ref=1"));
-		auto body = web::json::value(U("final_term=20"));
-		cli.make_request(methods::PUT, req.to_string(), body).then([](http_response response) {
-			EXPECT_EQ(response.status_code(), status_codes::OK);
-		}).wait();
+					EXPECT_EQ(result.serialize(), expected.serialize());
+				});
+			}).wait();
+		}
+		TEST_F(Crud, Score_OnUpdating_Success) {
+			auto req = uri_builder(local).append_path(U("score")).append_query(U("class_ref=1"));
+			auto body = web::json::value(U("final_term=20"));
+			cli.make_request(methods::PUT, req.to_string(), body).then([](http_response response) {
+				EXPECT_EQ(response.status_code(), status_codes::OK);
+			}).wait();
+		}
 	}
 }
--- src/Test/Smoke.cpp
+++ src/Test/Smoke.cpp
@@ -3,99 +3,101 @@
 #include "TestServer.h"
 
 namespace Test {
-	class Smoke : public ::testing::Test {
-	protected:
-		web::uri local = uri_builder(U("http://127.0.0.1:34568")).to_uri();
-		TestServer srv = TestServer(local, true);
-		BackEnd::Net::Client cli = BackEnd::Net::Client(local);
+	namespace End2End {
+		class Smoke : public ::testing::Test {
+		protected:
+			web::uri local = uri_builder(U("http://127.0.0.1:34568")).to_uri();
+			TestServer srv = TestServer(local, true);
+			BackEnd::Net::Client cli = BackEnd::Net::Client(local);
 
-		virtual void SetUp() {
-			srv.open();
-		}
+			virtual void SetUp() {
+				srv.open();
+			}
 
-		virtual void TearDown() {
-			srv.close();
-		}
-	};
+			virtual void TearDown() {
+				srv.close();
+			}
+		};
 
-	TEST_F(Smoke, Student_OnNotExisting_NotFound) {
-		auto req = uri_builder(local).append_path(U("student")).append_query(U("name=George"));
-		cli.make_request(methods::GET, req.to_string(), 0).then([](http_response response) {
-			EXPECT_EQ(response.status_code(), status_codes::NotFound);
-		}).wait();
-	}
-	
-	TEST_F(Smoke, Student_OnRequestingNotExistingEntity_BadRequest) {
-		auto req = uri_builder(local).append_path(U("students")).append_query(U("phone_number=09011234567"));
-		cli.make_request(methods::GET, req.to_string(), 0).then([](http_response response) {
-			EXPECT_EQ(response.status_code(), status_codes::BadRequest);
-		}).wait();
-	}
-
-	TEST_F(Smoke, Class_OnDictatingId_MethodNotAllowed) {
-		auto req = uri_builder(local).append_path(U("class")).append_query(U("class_id=4"))
-			.append_query(U("course_ref=1")).append_query(U("student_ref=1")).append_query(U("score_ref=null"));
-		cli.make_request(methods::POST, req.to_string(), 0).then([](http_response response) {
-			EXPECT_EQ(response.status_code(), status_codes::MethodNotAllowed); // MethodNotAllowed for dictating id of new entity
-		}).wait();
-	}
-
-	TEST_F(Smoke, Course_OnDoubleCreating_Conflict) {
-		auto req = uri_builder(local).append_path(U("course")).append_query(U("name=Physics"));
-		cli.make_request(methods::POST, req.to_string(), 0).then([](http_response response) {
-			EXPECT_EQ(response.status_code(), status_codes::Conflict); // Conflict for already existing data
-		}).wait();
-	}
-
-	TEST_F(Smoke, Score_OnUpdatingNonExisting_NotFound) {
-		auto req = uri_builder(local).append_path(U("score")).append_query(U("class_ref=4"));
-		auto body = web::json::value(U("final_term=20"));
-		cli.make_request(methods::PUT, req.to_string(), body).then([](http_response response) {
-			EXPECT_EQ(response.status_code(), status_codes::NotFound); // NotFound on put requst using a non-existing data
-		}).wait();
-	}
-	
-	TEST_F(Smoke, Student_OnRequestingHead_OK) {
-		auto req = uri_builder(local).append_path(U("student")).append_query(U("name=Ali"));
-		cli.make_request(methods::HEAD, req.to_string(), 0).then([](http_response response) {
-			EXPECT_EQ(response.status_code(), status_codes::OK); // OK for HEAD if the resource exist and valid
-		}).wait();
-	}
-	
-	TEST_F(Smoke, Class_OnRequestingOptions_OK) {
-		auto req = uri_builder(local).append_path(U("class")).append_query(U("student_ref=1"));
-		cli.make_request(methods::OPTIONS, req.to_string(), 0).then([](http_response response) {
-			EXPECT_EQ(response.status_code(), status_codes::OK); // OK for OPTIONS 
-			response.extract_json().then([](web::json::value result) { // Allowd http verbs on resource
-				web::json::value expected = web::json::value::parse(L"{\"Allow\":[\"GET\",\"POST\",\"PUT\",\"PATCH\",\"DEL\",\"OPTIONS\",\"HEAD\"]}");
-				EXPECT_EQ(result.serialize(), expected.serialize());
+		TEST_F(Smoke, Student_OnNotExisting_NotFound) {
+			auto req = uri_builder(local).append_path(U("student")).append_query(U("name=George"));
+			cli.make_request(methods::GET, req.to_string(), 0).then([](http_response response) {
+				EXPECT_EQ(response.status_code(), status_codes::NotFound);
 			}).wait();
-		}).wait();
+		}
+
+		TEST_F(Smoke, Student_OnRequestingNotExistingEntity_BadRequest) {
+			auto req = uri_builder(local).append_path(U("students")).append_query(U("phone_number=09011234567"));
+			cli.make_request(methods::GET, req.to_string(), 0).then([](http_response response) {
+				EXPECT_EQ(response.status_code(), status_codes::BadRequest);
+			}).wait();
+		}
+
+		TEST_F(Smoke, Class_OnDictatingId_MethodNotAllowed) {
+			auto req = uri_builder(local).append_path(U("class")).append_query(U("class_id=4"))
+				.append_query(U("course_ref=1")).append_query(U("student_ref=1")).append_query(U("score_ref=null"));
+			cli.make_request(methods::POST, req.to_string(), 0).then([](http_response response) {
+				EXPECT_EQ(response.status_code(), status_codes::MethodNotAllowed); // MethodNotAllowed for dictating id of new entity
+			}).wait();
+		}
+
+		TEST_F(Smoke, Course_OnDoubleCreating_Conflict) {
+			auto req = uri_builder(local).append_path(U("course")).append_query(U("name=Physics"));
+			cli.make_request(methods::POST, req.to_string(), 0).then([](http_response response) {
+				EXPECT_EQ(response.status_code(), status_codes::Conflict); // Conflict for already existing data
+			}).wait();
+		}
+
+		TEST_F(Smoke, Score_OnUpdatingNonExisting_NotFound) {
+			auto req = uri_builder(local).append_path(U("score")).append_query(U("class_ref=4"));
+			auto body = web::json::value(U("final_term=20"));
+			cli.make_request(methods::PUT, req.to_string(), body).then([](http_response response) {
+				EXPECT_EQ(response.status_code(), status_codes::NotFound); // NotFound on put requst using a non-existing data
+			}).wait();
+		}
+
+		TEST_F(Smoke, Student_OnRequestingHead_OK) {
+			auto req = uri_builder(local).append_path(U("student")).append_query(U("name=Ali"));
+			cli.make_request(methods::HEAD, req.to_string(), 0).then([](http_response response) {
+				EXPECT_EQ(response.status_code(), status_codes::OK); // OK for HEAD if the resource exist and valid
+			}).wait();
+		}
+
+		TEST_F(Smoke, Class_OnRequestingOptions_OK) {
+			auto req = uri_builder(local).append_path(U("class")).append_query(U("student_ref=1"));
+			cli.make_request(methods::OPTIONS, req.to_string(), 0).then([](http_response response) {
+				EXPECT_EQ(response.status_code(), status_codes::OK); // OK for OPTIONS 
+				response.extract_json().then([](web::json::value result) { // Allowd http verbs on resource
+					web::json::value expected = web::json::value::parse(L"{\"Allow\":[\"GET\",\"POST\",\"PUT\",\"PATCH\",\"DEL\",\"OPTIONS\",\"HEAD\"]}");
+					EXPECT_EQ(result.serialize(), expected.serialize());
+				}).wait();
+			}).wait();
+		}
+
+		TEST_F(Smoke, Course_OnDeletingInterconnected_Forbidden) {
+			auto req = uri_builder(local).append_path(U("course")).append_query(U("name=Math"));
+			cli.make_request(methods::DEL, req.to_string(), 0).then([](http_response response) {
+				EXPECT_EQ(response.status_code(), status_codes::Forbidden); // NO DELETE CASCADE !
+			}).wait();
+		}
+
+		TEST_F(Smoke, None_OnBareFullyNakedGetRequest_BadRequest) {
+			auto req = uri_builder(local);
+			cli.make_request(methods::GET, req.to_string(), 0).then([](http_response response) {
+				EXPECT_EQ(response.status_code(), status_codes::BadRequest);
+			}).wait();
+		}
+
+		// TODO: OK for TRACE: create a loopback for the request message
+
+		// TODO: stress_test to produce service_not_available
+
+		// TODO: authentication (base64 encoded not plain text)
+
+		// TODO: using cookies for authentication
+
+		// TODO: authorization
+
+		// TODO: caching (on front end??)
 	}
-
-	TEST_F(Smoke, Course_OnDeletingInterconnected_Forbidden) {
-		auto req = uri_builder(local).append_path(U("course")).append_query(U("name=Math"));
-		cli.make_request(methods::DEL, req.to_string(), 0).then([](http_response response) {
-			EXPECT_EQ(response.status_code(), status_codes::Forbidden); // NO DELETE CASCADE !
-		}).wait();
-	}
-
-	TEST_F(Smoke, None_OnBareFullyNakedGetRequest_BadRequest) {
-		auto req = uri_builder(local);
-		cli.make_request(methods::GET, req.to_string(), 0).then([](http_response response) {
-			EXPECT_EQ(response.status_code(), status_codes::BadRequest);
-		}).wait();
-	}
-
-	// TODO: OK for TRACE: create a loopback for the request message
-
-	// TODO: stress_test to produce service_not_available
-
-	// TODO: authentication (base64 encoded not plain text)
-
-	// TODO: using cookies for authentication
-
-	// TODO: authorization
-
-	// TODO: caching (on front end??)
 }
