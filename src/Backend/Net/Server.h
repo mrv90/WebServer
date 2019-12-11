10d67a98072a58427f944e35da797bbc39d14acf
mrv90 <hamid.mousavinasab@gmail.com>
2019-12-11 14:57:55 +0330

    ایجاد یک تسک برای بازتولید و انتقال فانکشنالیتی به کلاسی دیگر

--- src/Backend/Net/Server.h
+++ src/Backend/Net/Server.h
@@ -41,10 +41,11 @@ namespace BackEnd {
 			void answer_request(const int query_status, const web::http::http_request & req, const web::json::value & resp = NULL);
 			void answer_request(const web::http::http_request & req, const int http_code);
 			
-			bool contains_id(const web::http::http_request& req);
 			void print_current_date_time();
 			void print_requst_date(const web::http::http_request& req);
 			
+			// TODO refactore below functionality/methods to new class
+			bool contains_id(const web::http::http_request& req);
 			bool is_a_valid_request(const web::http::http_request& req);
 			bool could_have_fragments(const web::http::http_request& req);
 			bool must_have_atleast_one_fragment(const web::http::http_request& req);
