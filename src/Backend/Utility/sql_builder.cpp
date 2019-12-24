b55a999f565583bead3d7beb870fec0be33d306e
mrv90 <hamid.mousavinasab@gmail.com>
2019-12-24 19:32:29 +0330

    ویرایش جدید این کلاس؛ متدهای پابلیک عموما بازنویسی شده و قدیمیها به عنوان منسوخ علامتگزاری شده اند. تا حد متدها قدیمی حذف نشده بلکه نسخه جدید با امضای جدید ایجاد شده است.

--- src/Backend/Utility/sql_builder.cpp
+++ src/Backend/Utility/sql_builder.cpp
@@ -13,81 +13,54 @@ std::string sql_builder::to_select_query(const web::http::http_request& req)
 {
 	std::string url = utility::conversions::utf16_to_utf8(req.relative_uri().to_string());
 	std::string sql("SELECT ");
-	const boost::sregex_token_iterator end;
 	
-	const boost::regex opath_ptrn("(?<=(.\/))(\w+)"); // other pathes pattern or opath_ptrn
-	boost::sregex_token_iterator opath_itr(url.begin(), url.end(), opath_ptrn, 0);
-	auto start = opath_itr;
-	for (; opath_itr != end; opath_itr++)
-		*start != *opath_itr ? sql.append(*opath_itr + ", ") : sql.append(*opath_itr);
+	add_path_hierarchy(url, sql);
+	
 	if (sql.compare(sql.c_str()) == 0)
 		sql.append("* ");
 
-	const boost::regex path_ptrn("(?<=^\/)([a-zA-Z0-9]+)"); // main math or simply path
-	boost::smatch path_rslt;
-	boost::regex_search(url, path_rslt, path_ptrn);
-	sql.append("FROM ").append(path_rslt.empty() == false ? path_rslt[0].str() : "* ");
+	add_root_path(url, sql, "FROM ");
 
-	if (!req.relative_uri().query().empty()) {
-		const boost::regex q_ptrn("[a-zA-Z0-9_=-]+((?=&)|(?=$))"); // q or queries
-		boost::sregex_token_iterator q_itr(url.begin(), url.end(), q_ptrn, 0);
-		sql.append(" WHERE ");
-		start = q_itr;
-		for (; q_itr != end; q_itr++)
-			*start != *q_itr ? sql.append(" AND " + wrap_by_quotation(*q_itr)) : sql.append(wrap_by_quotation(*q_itr));
-	}
+	if (!req.relative_uri().query().empty())
+		add_pair_queries(url, sql, " WHERE ");
 
 	return sql;
 }
 
 std::string sql_builder::to_create_or_replace_cmd(const web::http::http_request& req)
 {
-	// TODO: add exception 
-	// TODO: ucout result of to_create
-	std::wstring sql(L"INSERT INTO ");
-	sql.append(req.request_uri().path().substr(1, req.request_uri().path().size() - 1));
-	
-	if (!req.request_uri().query().empty()) {
-		std::wstring qkey_buffer, qval_buffer = L"";
-		auto queries = req.request_uri().split_query(req.request_uri().query());
+	std::string url = utility::conversions::utf16_to_utf8(req.relative_uri().to_string());
+	std::string sql("INSERT ");
+	add_root_path(url, sql, "INTO ");
 
-		for (std::map<std::wstring, std::wstring>::iterator it = queries.begin(); it != queries.end(); it++) {
-			if (it != std::prev(queries.end())) {
-				qkey_buffer.append(it->first + L", ");
-				qval_buffer.append(L"'" + it->second + L"', ");
-			}
-			else {
-				qkey_buffer.append(it->first);
-				qval_buffer.append(L"'" + it->second + L"'");
-			}
-		}
-
-		sql.append(L"(" + qkey_buffer + L") VALUES(" + qval_buffer + L") ");
-	}
+	if (!req.relative_uri().query().empty())
+		add_value_of_queries(url, sql, " VALUES (");
 	
-	return std::string(sql.begin(), sql.end());
+	return sql.append(")");
 }
 
 std::string sql_builder::to_update_cmd(const web::http::http_request& req, const std::wstring& json_style_body)
 {
-	// TODO: add exception 
-	// TODO: ucout result of to_update
-	std::wstring sql(L"UPDATE ");
-	sql.append(req.request_uri().path().substr(1, req.request_uri().path().size() -1));
-	sql.append(L" SET " + convert_to_sql_format(json_style_body));// +L" WHERE ");
-	add_queries(req, sql);
-	return std::string(sql.begin(), sql.end());
+	const std::string url = utility::conversions::utf16_to_utf8(req.relative_uri().to_string());
+	std::string sql("UPDATE ");
+	
+	add_root_path(url, sql, "");
+	sql.append(" SET " + convert_to_sql_format(utility::conversions::utf16_to_utf8(json_style_body)));
+	add_pair_queries(url, sql, " WHERE ");
+
+	return sql;
 }
 
 std::string sql_builder::to_delete_cmd(const web::http::http_request& req)
 {
-	// TODO: add exception 
-	// TODO: ucout result of to_delete
-	std::wstring sql(L"DELETE FROM "); // no delete policy !
-	auto pathes = req.request_uri().split_path(req.request_uri().path());
-	add_pathes(req, sql);
-	add_queries(req, sql);
-	return std::string(sql.begin(), sql.end());
+	const std::string url = utility::conversions::utf16_to_utf8(req.relative_uri().to_string());
+	std::string sql("DELETE ");
+
+	add_root_path(url, sql, "FROM ");
+	/*add_path_hierarchy(url, sql);*/ // path hierarchy ??
+	add_pair_queries(url, sql, " WHERE ");
+
+	return sql;
 }
 
 std::wstring sql_builder::to_allowed_verbs(const web::http::http_request& req)
@@ -120,25 +93,68 @@ void sql_builder::add_pathes(const web::http::http_request& req, std::wstring& s
 	}
 }
 
+void sql_builder::add_root_path(const std::string &url, std::string &sql, const char* key_word)
+{
+	const boost::regex path_ptrn("(?<=^\/)([a-zA-Z0-9]+)"); // main math or simply path
+	boost::smatch path_rslt;
+	boost::regex_search(url, path_rslt, path_ptrn);
+	sql.append(key_word).append(path_rslt.empty() == false ? path_rslt[0].str() : "* ");
+}
+
 void sql_builder::add_queries(const web::http::http_request& req, std::wstring& sql)
 {
 	if (!req.request_uri().query().empty())
 		sql.append(L" WHERE " + wrap_by_quotation(req.request_uri().query()));
 }
 
+void sql_builder::add_pair_queries(const std::string &url, std::string &sql, const char* kew_word)
+{
+	const boost::regex ptrn("[a-zA-Z0-9_=-]+((?=&)|(?=$))");
+	boost::sregex_token_iterator it(url.begin(), url.end(), ptrn, 0);
+	const boost::sregex_token_iterator end;
+	sql.append(kew_word);
+	auto start = it;
+	for (; it != end; it++)
+		*start != *it ? sql.append(" AND " + wrap_by_quotation(*it)) : sql.append(wrap_by_quotation(*it));
+}
+
+void sql_builder::add_value_of_queries(const std::string &url, std::string &sql, const char* kew_word) {
+
+	const boost::regex ptrn("((?<==)[a-zA-Z0-9._-]+)");
+	boost::sregex_token_iterator it(url.begin(), url.end(), ptrn, 0);
+	const boost::sregex_token_iterator end;
+	sql.append(kew_word);
+	auto start = it;
+	for (; it != end; it++) {
+		*start != *it ? sql.append(", '" + *it + "'") : sql.append("'" + *it + "'");
+	}
+}
+
+void sql_builder::add_path_hierarchy(const std::string &url, std::string &sql)
+{
+	const boost::regex opath_ptrn("(?<=(.\/))(\w+)"); // other pathes pattern
+	boost::sregex_token_iterator opath_itr(url.begin(), url.end(), opath_ptrn, 0);
+	const boost::sregex_token_iterator end;
+
+	auto start = opath_itr;
+	for (; opath_itr != end; opath_itr++) {
+		*start != *opath_itr ? sql.append(*opath_itr + ", ") : sql.append(*opath_itr);
+	}
+}
+
 std::wstring sql_builder::wrap_by_quotation(const std::wstring& equation)
 {
-	boost::regex after_equal_sign("((?<==)[a-zA-Z0-9.]+)"); // we have float in data; 17.5 has single dot
+	boost::regex after_equal_sign("((?<==)[a-zA-Z0-9._-]+)"); // we have float: 17.5 has single dot
 	std::string result = boost::regex_replace(
-		std::string(equation.begin(), equation.end()), after_equal_sign, "'$0'");
+		std::string(equation.begin(), equation.end()), after_equal_sign, "'$&'");
 
 	return std::wstring(result.begin(), result.end());
 }
 
 std::string sql_builder::wrap_by_quotation(const std::string& equation)
 {
-	boost::regex after_equal_sign("((?<==)[a-zA-Z0-9.]+)"); // we have float in data; 17.5 has single dot
-	return boost::regex_replace(equation, after_equal_sign, "'$0'");
+	boost::regex after_equal_sign("((?<==)[a-zA-Z0-9._-]+)"); // we have float: 17.5 has single dot
+	return boost::regex_replace(equation, after_equal_sign, "'$&'");
 }
 
 bool sql_builder::primary_key_requested(const web::http::http_request & req)
@@ -163,4 +179,17 @@ std::wstring sql_builder::convert_to_sql_format(const std::wstring& json_style) 
 
 	std::string result = key_result[0].str() + "='" + val_result[0].str() + "'";
 	return std::wstring(result.begin(), result.end());
+}
+
+std::string sql_builder::convert_to_sql_format(const std::string& json_style) {
+	std::string body = std::string(json_style.begin(), json_style.end());
+	boost::regex key("[a-zA-Z0-9_]+(?=\":)");
+	boost::regex val("[a-zA-Z0-9_]+(?=(\"}|}))");
+	boost::smatch key_result;
+	boost::smatch val_result;
+
+	boost::regex_search(body, key_result, key);
+	boost::regex_search(body, val_result, val);
+
+	return key_result[0].str() + "='" + val_result[0].str() + "'";
 }
--- src/Backend/Utility/sql_builder.h
+++ src/Backend/Utility/sql_builder.h
@@ -15,10 +15,17 @@ public:
 	std::wstring to_allowed_verbs(const web::http::http_request& req);
 
 private:
+	[[deprecated("This method replaced by add_root_path and add path_hierarchy")]]
 	void add_pathes(const web::http::http_request& req, std::wstring &sql);
+	void add_root_path(const std::string &url, std::string &sql, const char* key_word);
+	void add_path_hierarchy(const std::string &url, std::string &sql);
+	[[deprecated("This method replaced by add_pair_queries and add_value_of_queries")]]
 	void add_queries(const web::http::http_request& req, std::wstring &sql);
+	void add_pair_queries(const std::string &url, std::string &sql, const char* kew_word);
+	void add_value_of_queries(const std::string &url, std::string &sql, const char* kew_word);
 	std::wstring wrap_by_quotation(const std::wstring& equation);
 	std::string wrap_by_quotation(const std::string& equation);
 	bool primary_key_requested(const web::http::http_request& req);
 	std::wstring convert_to_sql_format(const std::wstring& json_style);
+	std::string convert_to_sql_format(const std::string& json_style);
 };
